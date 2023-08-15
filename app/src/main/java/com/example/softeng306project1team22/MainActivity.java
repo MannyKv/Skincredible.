4d4994fd74c3c11a4a0c1095b2727afc24974466
atharvaarankalle <atharvanz@gmail.com>
2023-08-15 16:16:20 +1200

    fix: Removed unnecessary code

--- app/src/main/java/com/example/softeng306project1team22/MainActivity.java
+++ app/src/main/java/com/example/softeng306project1team22/MainActivity.java
@@ -4,19 +4,11 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.softeng306project1team22.Data.CleanserDataProvider;
-import com.example.softeng306project1team22.Data.MoisturiserDataProvider;
-import com.example.softeng306project1team22.Data.SunscreenDataProvider;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        CleanserDataProvider.addCleansersToFirestore();
-        MoisturiserDataProvider.addMoisturisersToFirestore();
-        SunscreenDataProvider.addSunscreenToFirestore();
     }
 }
