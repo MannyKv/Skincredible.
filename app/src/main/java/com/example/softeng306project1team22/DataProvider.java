8399ba68eca8cb8214eb5b9c74fb02f7ce6bcdac
Mlal371 <mlal371@aucklanduni.ac.nz>
2023-08-25 20:50:59 +1200

    java-docs/ Created java docs and comments for all of the DataProvider methods

--- app/src/main/java/com/example/softeng306project1team22/DataProvider.java
+++ app/src/main/java/com/example/softeng306project1team22/DataProvider.java
@@ -19,12 +19,22 @@ import java.util.concurrent.CompletableFuture;
 public class DataProvider {
     static List<Category> categories = new ArrayList<>();
     static List<IItem> allItems = new ArrayList<>();
-    //static IItem recievedItems;
 
+    /**
+     * returns the list of categories
+     *
+     * @return list of categories
+     */
     public static List<Category> getCategories() {
         return categories;
     }
 
+    /**
+     * Retrieves a category by id from the list of categories
+     *
+     * @param id
+     * @return the category specified by id
+     */
     public static Category getCategoryById(String id) {
         for (Category category : categories) {
             if (category.getId().equals(id)) {
@@ -34,31 +44,41 @@ public class DataProvider {
         return null;
     }
 
+    /**
+     * A getter for the allItem list variable.
+     * If the item list is empty then it will make the fetchAllItems() call and return that as a completeable future
+     *
+     * @return list of ALL items future
+     */
     public static CompletableFuture<List<IItem>> getAllItems() {
         CompletableFuture<List<IItem>> future = new CompletableFuture<>();
         if (allItems.size() > 0) {
-            future.complete(allItems);
+            future.complete(allItems); //return if list is full
             return future;
         } else {
-            fetchAllItems().thenAccept(allItems -> future.complete(allItems));
+            fetchAllItems().thenAccept(allItems -> future.complete(allItems)); //make the database query
         }
         return future;
     }
 
-
+    /**
+     * Fetches all the category data in the category collection on firebase
+     *
+     * @return a list of categories as a completeable future
+     */
     public static CompletableFuture<List<Category>> fetchCategoryData() {
-        categories.clear();
-        CompletableFuture<List<Category>> future = new CompletableFuture<>();
+        categories.clear(); //clear the list if its not already
+        CompletableFuture<List<Category>> future = new CompletableFuture<>(); //new completeable future
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         CollectionReference collectionRef = db.collection("category");
-
+        //make the database call using Task
         collectionRef.get().addOnSuccessListener(queryDocumentSnapshots -> {
             Log.d("Firestore", "Data retrieved successfully");
             for (DocumentSnapshot document : queryDocumentSnapshots.getDocuments()) {
                 categories.add(new Category(document.get("name", String.class), document.getId(), document.get("imageName", String.class)));
-                System.out.println("Data in categories: " + categories.get(0).getName());
+                //added category to list
             }
-            future.complete(categories);
+            future.complete(categories); //complete the future
 
         }).addOnFailureListener(e -> {
             System.out.println("Category Data Retrieval Failure");
@@ -68,20 +88,24 @@ public class DataProvider {
         return future;
     }
 
+    /**
+     * A method to fetch all items from all category collections in the database
+     *
+     * @return CompleteableFuture with a list of ALL items
+     */
     public static CompletableFuture<List<IItem>> fetchAllItems() {
 
         FirebaseFirestore dbs = FirebaseFirestore.getInstance();
-
+        //create a ref for all the collections
         CollectionReference colRef1 = dbs.collection("cleanser");
         CollectionReference colRef2 = dbs.collection("moisturiser");
         CollectionReference colRef3 = dbs.collection("sunscreen");
-
+        //create a completeable future for all items and call the retriever
         CompletableFuture<List<IItem>> fetchCleanser = retrieveFromCollection(colRef1, Cleanser.class);
         CompletableFuture<List<IItem>> fetchMoisturiser = retrieveFromCollection(colRef2, Moisturiser.class);
         CompletableFuture<List<IItem>> fetchSunscreen = retrieveFromCollection(colRef3, Sunscreen.class);
         CompletableFuture<Void> combinedFuture = CompletableFuture.allOf(fetchCleanser, fetchMoisturiser, fetchSunscreen);
-        CompletableFuture<List<IItem>> future = combinedFuture.thenApply(voidResult -> {
-            //List<IItem> allItems = new ArrayList<>();
+        CompletableFuture<List<IItem>> future = combinedFuture.thenApply(voidResult -> { //create a new combined future to the future only returns once everything has been retrieved
             allItems.addAll(fetchCleanser.join());
             allItems.addAll(fetchMoisturiser.join());
             allItems.addAll(fetchSunscreen.join());
@@ -91,6 +115,13 @@ public class DataProvider {
         return future;
     }
 
+    /**
+     * Retrieves a collection of items based on the reference and the Item class type
+     *
+     * @param colRef    a collection reference of the items
+     * @param itemClass the type of item class
+     * @return a compeleteable future of a list of items from that category
+     */
     public static CompletableFuture<List<IItem>> retrieveFromCollection(CollectionReference colRef, Class<?> itemClass) {
         CompletableFuture<List<IItem>> fetchFuture = new CompletableFuture<>();
         List<IItem> items = new ArrayList<>();
@@ -99,13 +130,21 @@ public class DataProvider {
             for (DocumentSnapshot documentSnapshot : queryDocumentSnapshots.getDocuments()) {
                 IItem item = (IItem) documentSnapshot.toObject(itemClass);
                 items.add(item);
-
+                //add the retrived item to the list
             }
-            fetchFuture.complete(items);
+            fetchFuture.complete(items); //complete the future
         });
         return fetchFuture;
     }
 
+    /**
+     * Fetches an item from firebase based on a product category and product id
+     * Passes it back as a completable future which the calling method should prepare for
+     *
+     * @param productCategory the category the product is in
+     * @param productId       the id of the product
+     * @return a completeable future IItem
+     */
     public static CompletableFuture<IItem> fetchItemById(String productCategory, String productId) {
         CompletableFuture<IItem> future = new CompletableFuture<>();
         FirebaseFirestore database = FirebaseFirestore.getInstance();
@@ -113,7 +152,7 @@ public class DataProvider {
         database.collection(productCategory.toLowerCase()).document(productId)
                 .get()
                 .addOnSuccessListener(documentSnapshot -> {
-                    switch (productCategory.toLowerCase()) {
+                    switch (productCategory.toLowerCase()) { //fetching by the type
                         case "sunscreen":
                             IItem recievedItems = (IItem) documentSnapshot.toObject(Sunscreen.class);
                             future.complete(recievedItems);
@@ -127,25 +166,26 @@ public class DataProvider {
                             future.complete(recievedItems2);
                     }
 
-                    //future.complete(recievedItems);
                 });
         return future;
     }
 
+    /**
+     * Returns a CompletableFuture to the calling class with a List of the recently viewed Items
+     * Utilises fetchItemById() to get each specific document within the firetore collection
+     *
+     * @return A completeable future that will contain a list of recently viewed items
+     */
     public static CompletableFuture<List<IItem>> fetchRecentlyViewed() {
         CompletableFuture<List<IItem>> future = new CompletableFuture<>();
         FirebaseFirestore dbs = FirebaseFirestore.getInstance();
         CollectionReference colRef = dbs.collection("recently-viewed");
-        List<IItem> recentlyViewed = new ArrayList<>();
+        List<IItem> recentlyViewed = new ArrayList<>(9);
         colRef.orderBy("timeAdded", Query.Direction.DESCENDING).get().addOnSuccessListener(queryDocumentSnapshots -> {
             Log.d("Firestore", "Recently viewed retrieved successfully");
-
-            List<CompletableFuture<IItem>> fetchFutures = new ArrayList<>();
             for (DocumentSnapshot documentSnapshot : queryDocumentSnapshots.getDocuments()) {
                 String id = documentSnapshot.getString("itemId");
                 String categoryName = documentSnapshot.getString("categoryName");
-                //CollectionReference itemRef;
-
 
                 fetchItemById(categoryName, id).thenAccept(item -> {
                     recentlyViewed.add(item);
@@ -155,10 +195,7 @@ public class DataProvider {
                     }
                 });
             }
-
-
         });
-        System.out.println("Fetch Recent Compleyed");
         return future;
     }
 
